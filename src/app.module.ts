9c4d61705922609db080104964cf9cc52b7020f0
Moustafa Mahmoud Shadi <moustafa.mahmoud@student.giu-uni.de>
2024-11-24 01:47:05 +0200

    moustafa shadi mabye3rafsh yestakhdem git.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminModule } from './admin/admin.module';
 import { UserModule } from './user/user.module';
-import { InstructorModule } from './instructor/instructor.module';
-import { StudentModule } from './student/student.module';
 import { AnalyticsModule } from './analytics/analytics.module';
-import { ChatModule } from './chat/chat.module';
-import { QuickNotesModule } from './quick-notes/quick-notes.module';
-import { ForumModule } from './forum/forum.module';
+import { QuickNotesModule } from './notes/quick-notes.module';
 import { ProgressModule } from './progress/progress.module';
-import { ResponsessModule } from './responsess/responsess.module';
-import { ResponsesModule } from './responses/responses.module';
-import { ModulesModule } from './modules/modules.module';
+import { ResponsesModule } from './response/response.module';
+import { ModulesModule } from './module/module.module';
 import { CommunicationModule } from './communication/communication.module';
 
+
 @Module({
-  imports: [AdminModule, UserModule, InstructorModule, StudentModule, AnalyticsModule, ChatModule, QuickNotesModule, ForumModule, ProgressModule, ResponsessModule, ResponsesModule, ModulesModule, CommunicationModule],
+  imports: [UserModule, AnalyticsModule,  QuickNotesModule, ProgressModule,  ResponsesModule, ModulesModule, CommunicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
