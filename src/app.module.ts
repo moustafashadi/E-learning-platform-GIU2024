97e7c0b5322cec4c4a6722d05da95579f77afd0c
omarsuff <omarwaleedalazhary@gmail.com>
2024-11-23 22:07:19 +0200

    done with the done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { UserModule } from './user/user.module';
+import { InstructorModule } from './instructor/instructor.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, UserModule, InstructorModule, StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
