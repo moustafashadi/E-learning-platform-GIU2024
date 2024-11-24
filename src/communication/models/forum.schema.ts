757af66dce2035ec6847505bc7f7b227df83923e
yahia1917 <yahiasamer2004@gmail.com>
2024-11-24 20:47:43 +0200

    keda forum, chat w notifications schema

---
+++ src/communication/models/forum.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type ForumDocument = Forum & Document;
+
+@Schema({ timestamps: true })
+export class Forum {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  createdBy: Types.ObjectId;
+
+  @Prop({ type: [{ type: String }] })
+  tags: string[];
+}
+
+export const ForumSchema = SchemaFactory.createForClass(Forum);
