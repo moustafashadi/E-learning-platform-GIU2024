6673467df1fe2bc3e4179d62e1f53eebc688a0b7
Moustafa Mahmoud Shadi <moustafa.mahmoud@student.giu-uni.de>
2024-11-24 22:09:32 +0200

    updated admin (notification property)

--- src/user/models/admin.schema.ts
+++ src/user/models/admin.schema.ts
@@ -1,14 +1,15 @@
 import { Prop , Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, Schema as MongooseSchema } from "mongoose";
 import { User } from "./user.schema";
+import { Notification, NotificationSchema } from "../../communication/models/notification.schema";
 
 @Schema()
 export class Admin extends User {
     @Prop({type: []})
     SystemLogs: string[];
 
-    @Prop({type: [{type: MongooseSchema.Types.ObjectId, ref: 'Notification'}]})
-    announcements : MongooseSchema.Types.ObjectId[];
+    @Prop({type: NotificationSchema})
+    announcements : Notification[];
 }
 
 export type AdminDocument = Admin & Document;
