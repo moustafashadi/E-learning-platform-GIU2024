c341b15568c56be84d27b9baf8f187f3b91402c8
Moustafa Mahmoud Shadi <moustafa.mahmoud@student.giu-uni.de>
2024-11-24 21:19:07 +0200

    analytics module done. all other modules left

--- src/notes/models/notes.schema.ts
+++ src/notes/models/notes.schema.ts
@@ -1,16 +1,14 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import {Schema as MongooseSchema} from 'mongoose'
 import mongoose, { Document } from 'mongoose';
-import {Course} from 'src\course\models\course.schema.ts'
-import {User} from 'src\user\models\user.schema.ts'
 
 @Schema({ timestamps: { createdAt: 'created_at', updatedAt: 'last_updated' } })
 export class Note {
   
-  @Prop({type: { type:MongooseSchema.Types.ObjectId , ref:User } })
+  @Prop({type: { type:MongooseSchema.Types.ObjectId , ref:'User' } })
     userId: MongooseSchema.Types.ObjectId;
     
-  @Prop({type: { type:MongooseSchema.Types.ObjectId , ref:Course } })
+  @Prop({type: { type:MongooseSchema.Types.ObjectId , ref:'Course' } })
     courseId: MongooseSchema.Types.ObjectId;
   
   @Prop({ required: true })
--- src/response/models/response.schema.ts
+++ src/response/models/response.schema.ts
@@ -1,16 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, {  Document, Mongoose} from 'mongoose';
 import {Schema as MongooseSchema} from 'mongoose'
-import {User} from 'src\user\models\user.schema.ts'
-import {Quiz} from 'src\quiz\models\quiz.schema.ts'
+
 @Schema({ timestamps: { createdAt: 'submitted_at' } })
 
 export class Responses{
     
-    @Prop({type: { type:MongooseSchema.Types.ObjectId , ref:User } })
+    @Prop({type: { type:MongooseSchema.Types.ObjectId , ref: 'User' } })
     userId: MongooseSchema.Types.ObjectId;
 
-    @Prop( {type: { type:MongooseSchema.Types.ObjectId , ref:Quiz }})
+    @Prop( {type: { type:MongooseSchema.Types.ObjectId , ref: 'Quiz' }})
     QuizId: MongooseSchema.Types.ObjectId;
 
     @Prop({ type: [{ type: Object }] })
