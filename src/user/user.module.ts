f6b40fca024961ecc7c759507a4d54688add2792
omarsuff <omarwaleedalazhary@gmail.com>
2024-12-02 21:13:45 +0200

    auth update

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './models/user.schema'; 
-import { UserDocument } from './models/user.schema'; 
-
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-    MongooseModule.forFeature([{ name: 'Admin', schema: UserSchema }]),
-    MongooseModule.forFeature([{ name: 'Instructor', schema: UserSchema }]),
-    MongooseModule.forFeature([{ name: 'Student', schema: UserSchema }]),
+    MongooseModule.forFeature([//registering the user schema with mongoose
+      { name: 'User', schema: UserSchema },
+      { name: 'Admin', schema: UserSchema },
+      { name: 'Instructor', schema: UserSchema },
+      { name: 'Student', schema: UserSchema },
+    ]),
   ],
   controllers: [],
   providers: [],
