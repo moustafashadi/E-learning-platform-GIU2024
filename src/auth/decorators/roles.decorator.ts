64d9ea169292379f1fd70b9bd695f3e051d7dacf
Youssef <youssef.amr.04@gmail.com>
2024-12-11 19:32:49 +0200

    main update

--- src/auth/decorators/roles.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-export const ROLES_KEY = 'roles';
-export const Roles = 
-(...roles: Role[]) =>SetMetadata(ROLES_KEY, roles);
-export enum Role {
-    User = 'student',
-    Admin = 'admin',
-    Instructor = 'instructor'
-  }
