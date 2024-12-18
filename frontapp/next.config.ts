6982688e0c5995986c7c38a890087dcc7ffd478c
Moustafa Mahmoud Shadi <moustafa.mahmoud@student.giu-uni.de>
2024-12-19 01:03:08 +0200

    message

--- frontapp/next.config.ts
+++ frontapp/next.config.ts
@@ -1,13 +1,17 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { NextConfig } from 'next';
+
+/** @type {NextConfig} */
+const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   async rewrites() {
     return [
       {
         source: "/auth/:path*",
         destination: "http://localhost:3000/auth/:path*", // Proxy to Nest.js backend
+      },
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3000/api/:path*", // Proxy to Nest.js backend
       },
     ];
   },
